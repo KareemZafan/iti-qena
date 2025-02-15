a82985506119b46ac51d2979b791a8afa08d7631
KareemZafan <kareemm.zafan@gmail.com>
2025-02-15 11:05:04 +0200

    Add new sqrt function

--- src/main.c
+++ src/main.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 
+#include<math.h>
+
+long getSquareRoot(long value){
+  return sqrt(value); 
+}
 
 int main(){
 
@@ -12,5 +17,7 @@ int b = 20
 
  for(int i = 0 ; i < 10, i++)
   printf("%d",i + 1); 
+  
+ printf("Square root for %d = %d",100,getSquareRoot(100));   
 
 return 0;}
