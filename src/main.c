f5c9ea1196620d19ee46d9d025470b8cc02622a8
KareemZafan <kareemm.zafan@gmail.com>
2025-02-15 11:40:32 +0200

    Removing Ahmed's task

--- src/main.c
+++ src/main.c
@@ -13,11 +13,6 @@ int b = 20
 
  printf("%d", a + b );
 
- // Ahmed Task 
-
- for(int i = 0 ; i < 10, i++)
-  printf("%d",i + 1); 
-  
  printf("Square root for %d = %d",100,getSquareRoot(100));   
 
 return 0;}
